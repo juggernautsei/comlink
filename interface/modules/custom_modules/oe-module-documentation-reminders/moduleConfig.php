c88dd0a28e271f41aec4f94cded59110d8aec088
Sherwin <sherwingaddis@gmail.com>
2022-03-25 12:27:42 -0400

    Initial Upload for documentation reminder

---
+++ interface/modules/custom_modules/oe-module-documentation-reminders/moduleConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ *  package   OpenEMR
+ *  link      https://affordablecustomehr.com
+ *  author    Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  copyright Copyright (c )2021. Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  All rights reserved
+ *
+ */
+
+
+$module_config = 1;
+
+require_once('./welcome.php');
---
+++ interface/modules/custom_modules/oe-module-documentation-reminders/openemr.bootstrap.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ *
+ *  package   OpenEMR
+ *  link      https://affordablecustomehr.com
+ *  author    Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  copyright Copyright (c )2021. Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  All rights reserved
+ *
+ */
+
+namespace Juggernaut\Modules\DocumentReminder;
+
+use OpenEMR\Menu\MenuEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+function oe_module_documentation_reminders_add_menu_item(MenuEvent $event)
+{
+    $menu = $event->getMenu();
+
+    $menuItem = new stdClass();
+    $menuItem->requirement = 0;
+    $menuItem->target = 'mod';
+    $menuItem->menu_id = 'docr0';
+    $menuItem->label = xlt("Documentation Reminders");
+    $menuItem->url = "/interface/modules/custom_modules/oe-module-documentation-reminders/public/";
+    $menuItem->children = [];
+    $menuItem->acl_req = ["patients", "docs"];
+    $menuItem->global_req = [];
+
+    foreach ($menu as $item) {
+        if ($item->menu_id == 'clcimg') {
+            $item->children[] = $menuItem;
+            break;
+        }
+    }
+
+    $event->setMenu($menu);
+
+    return $event;
+}
+
+/**
+ * @var EventDispatcherInterface $eventDispatcher
+ * @var array                    $module
+ * @global                       $eventDispatcher @see ModulesApplication::loadCustomModule
+ * @global                       $module          @see ModulesApplication::loadCustomModule
+ */
+
+$eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'oe_module_documentation_reminders_add_menu_item');
