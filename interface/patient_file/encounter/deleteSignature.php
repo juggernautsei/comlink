26c0a799a28635bd242d676e67ddb9e6c4cf712d
Sherwin <sherwingaddis@gmail.com>
2022-04-20 15:17:11 -0400

    deleting signature from encounter

--- interface/patient_file/encounter/deleteSignature.php
+++ interface/patient_file/encounter/deleteSignature.php
@@ -8,9 +8,11 @@
  *  license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+require_once dirname(__FILE__, 3) . "/globals.php";
+
 echo $_GET['encounter'];
 
 
 $ssql = "DELETE FROM form_encounter WHERE `encounter` = ? ";
 
-sqlStatement($ssql, [$_GET['encounter']]);
+sqlQuery($ssql, [$_GET['encounter']]);
