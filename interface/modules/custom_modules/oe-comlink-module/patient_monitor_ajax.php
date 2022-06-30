6e3a95567320e141dd6008cfa4cc433445cd775d
Sherwin <sherwingaddis@gmail.com>
2022-06-29 20:04:32 -0400

    Adding the auto alert if the BP is too high

--- interface/modules/custom_modules/oe-comlink-module/patient_monitor_ajax.php
+++ interface/modules/custom_modules/oe-comlink-module/patient_monitor_ajax.php
@@ -40,14 +40,17 @@ while ($row = sqlFetchArray($res)) {
             }
         }
 
-        if ($row['alert'] == "Need Attention") {
-            $alert = '<div class="alert alert-danger" role="alert">' . $row['alert'] . '</div>';
-        } elseif ( $row['alert'] == "Monitored") {
+        $bpUpper = explode("/", $row['bp_upper']);
+        $bpLower = explode("/", $row['bp_lower']);
+
+        if (($form_vitalsrow['bpd'] > $bpUpper[0]) || ($form_vitalsrow['bps'] > $bpUpper[1])) {
+            $alert = '<div class="alert alert-info" role="alert"> Needs Attention </div>';
+        } elseif (($form_vitalsrow['bpd'] < $bpLower[0]) || ($form_vitalsrow['bps'] < $bpLower[1]) ) {
+            $alert = '<div class="alert alert-info" role="alert"> Needs Attention </div>';
+        } elseif  ($row['alert'] == "Need Attention") {
             $alert = '<div class="alert alert-info" role="alert">'.$row['alert'] . '</div>';
-        } elseif  ($form_vitalsrow['bps'] > $row['bs_upper']) {
-            $alert = '<div class="alert alert-info" role="alert"> Needs Attention </div>';
-        }  elseif  ($form_vitalsrow['bpd'] > $row['bs_lower']) {
-            $alert = '<div class="alert alert-info" role="alert"> Needs Attention </div>';
+        }  elseif  ( $row['alert'] == "Monitored") {
+            $alert = '<div class="alert alert-danger" role="alert">' . $row['alert'] . '</div>';
         } else {
             $alert='';
         }
